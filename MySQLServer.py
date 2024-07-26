f72cac03077a4f860b12a12bcd94f7b9169540c7
Meshack <rutomeshack18@gmail.com>
2024-07-26 08:44:03 +0300

    Some minor changes

--- MySQLServer.py
+++ MySQLServer.py
@@ -3,15 +3,15 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host = 'localhost',
     user = 'root',
-    password = 'Mr@11731103',
+    password = 'password',
 )
 
 mycursor = mydb.cursor()
 try:
     mycursor.execute('CREATE DATABASE IF NOT EXISTS alx_book_store')
     print("Database 'alx_book_store' created successfully!")
-except Exception as e:
-    print("Ane error occured: {e}")
+except mysql.connector.Error as e:
+    print("An error occured: {e}")
 
 mycursor.close()
 mydb.close()
--- task_2.sql
+++ task_2.sql
@@ -3,9 +3,10 @@ USE DATABASE alx_book_store;
 CREATE TABLE IF NOT EXISTS Books(
     book_id PRIMARY KEY,
     title VARCHAR(130),
-    author_id FOREIGN KEY REFERENCES Authors(author_id),
+    author_id INT,
     price DOUBLE,
     publication_date DATE
+    FOREIGN KEY (author_id)  REFERENCES Authors(author_id),
 );
 
 CREATE TABLE IF NOT EXISTS Authors(
@@ -21,14 +22,18 @@ CREATE TABLE IF NOT EXISTS Customers(
 );
 
 CREATE TABLE IF NOT EXISTS Orders(
-    order_id PRIMARY KEY,
-    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
-    order_date DATE
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE IF NOT EXISTS Order_Details(
     orderdetail_id PRIMARY KEY,
-    order_id FOREIGN KEY REFERENCES Orders(order_id),
-    FOREIGN KEY (book_id)  REFERENCES Books(book_id),
-    quantity DOUBLE
+    book_id INT,
+    order_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id)  REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id)  REFERENCES Books(book_id)
+    
 );
