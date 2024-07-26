a89c47e6cf3aced8c550f70714eb3a3424c90207
Meshack <rutomeshack18@gmail.com>
2024-07-26 07:23:01 +0300

    More changes

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -24,13 +24,13 @@ CREATE TABLE IF NOT EXISTS Customers(
 
 CREATE TABLE IF NOT EXISTS Orders(
     order_id PRIMARY KEY,
-    customer_id FOREIGN KEY REFERENCES Customers(customer_id),
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
     order_date DATE
 );
 
 CREATE TABLE IF NOT EXISTS Order_Details(
     orderdetail_id PRIMARY KEY,
     order_id FOREIGN KEY REFERENCES Orders(order_id),
-    book_id FOREIGN KEY REFERENCES Books(book_id),
+    FOREIGN KEY (book_id)  REFERENCES Books(book_id),
     quantity DOUBLE
 );
